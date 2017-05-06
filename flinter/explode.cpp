7782cca0e9eb2fe81f8ae935e22fcd4eb10dffb9
Yiyuan Zhong <yiyuanzhong@gmail.com>
2017-05-06 21:36:17 +0800

    explode: parse number lists

--- flinter/explode.cpp
+++ flinter/explode.cpp
@@ -16,6 +16,7 @@
 #include "flinter/explode.h"
 
 #include <ctype.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include <algorithm>
@@ -87,6 +88,130 @@ void explode(const std::string &methods,
 
         pos = hit + 1;
     }
+}
+
+static const char *explode_list_internal(const char *s,
+                                         std::vector<int> *o,
+                                         int max)
+{
+    enum {
+        NONE,
+        DASH,
+        COMMA,
+        DIGIT,
+        RANGE,
+    } last = NONE;
+    int digit = -1;
+
+    for (/* Nothing */; /* Nothing */; /* Nothing */) {
+        if (*s >= '0' && *s <= '9') {
+            char *end;
+            unsigned long n;
+            if (last != COMMA && last != DASH && last != NONE) {
+                return NULL;
+            }
+
+            n = strtoul(s, &end, 10);
+            if (s == end || n > static_cast<unsigned long>(max)) {
+                return NULL;
+            }
+
+            int now = static_cast<int>(n);
+            if (last == DASH) {
+                if (digit > now) {
+                    return NULL;
+                }
+
+                last = RANGE;
+                for (int i = digit; i <= now; ++i) {
+                    o->push_back(i);
+                }
+
+            } else {
+                last = DIGIT;
+                digit = now;
+            }
+
+            s = end;
+
+        } else if (*s == ',') {
+            if (last != DIGIT && last != RANGE) {
+                return NULL;
+            }
+
+            if (last == DIGIT) {
+                o->push_back(digit);
+            }
+
+            last = COMMA;
+            ++s;
+
+        } else if (*s == '-') {
+            if (last != DIGIT) {
+                return NULL;
+            }
+
+            last = DASH;
+            ++s;
+
+        } else if (*s == ' ' || *s == '\t') {
+            ++s;
+
+        } else if (*s == ';' || *s == '\0') {
+            if (last == DIGIT) {
+                o->push_back(digit);
+            } else if (last != RANGE && last != NONE) {
+                return NULL;
+            }
+
+            ++s;
+            break;
+
+        } else {
+            return NULL;
+        }
+    }
+
+    std::sort(o->begin(), o->end());
+    std::vector<int>::iterator ne = std::unique(o->begin(), o->end());
+    o->erase(ne, o->end());
+    return s;
+}
+
+int explode_list(const std::string &input,
+                 std::vector<int> *output,
+                 int max)
+{
+    if (!output || max < 0) {
+        return -1;
+    }
+
+    output->clear();
+    return explode_list_internal(input.c_str(), output, max) ? 0 : -1;
+}
+
+int explode_lists(const std::string &input,
+                  std::vector<std::vector<int> > *output,
+                  int max)
+{
+    if (!output || max < 0) {
+        return -1;
+    }
+
+    output->clear();
+    const char *s = input.c_str();
+    const char *e = s + input.length();
+    while (s <= e) {
+        std::vector<int> one;
+        s = explode_list_internal(s, &one, max);
+        if (!s) {
+            return -1;
+        }
+
+        output->push_back(one);
+    }
+
+    return 0;
 }
 
 } // namespace flinter
--- flinter/explode.h
+++ flinter/explode.h
@@ -37,6 +37,23 @@ extern void explode(const std::string &methods,
                     const char *delim,
                     std::set<std::string> *result);
 
+// A list contains non-negative elements.
+// Comma separated digits or ranges.
+// For example: 1,3-6,8
+extern int explode_list(const std::string &input,
+                        std::vector<int> *output,
+                        int max);
+
+// Semicolon separated lists.
+// For example: 1,3;1;2
+// Note that empty lists remain in the result.
+// ""   : one list
+// "1"  : one list
+// "1;" : two lists
+extern int explode_lists(const std::string &input,
+                         std::vector<std::vector<int> > *output,
+                         int max);
+
 template <class iterator>
 inline void implode(const std::string &glue,
                     iterator begin, iterator end,
